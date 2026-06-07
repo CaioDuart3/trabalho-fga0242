87ea54a4e07aaf05dda6fab7a4f184f307137235
caioduart3 <caioduarte330@gmail.com>
2026-06-06 21:02:57 -0300

    test: add classe de teste parametrizada para diferenças de grafia

--- projeto/src/test/java/entities/TestDifferencasGrafia.java
+++ projeto/src/test/java/entities/TestDifferencasGrafia.java
@@ -1,8 +1,51 @@
 package entities;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import tppe.entities.DifferencasGrafia;
+
 /**
- * Suite de testes para o Caso 1: Diferenças de grafia (tipográficas)
+ * Suite de testes para o Caso 1: Diferencas de grafia (tipograficas).
  */
+@RunWith(Parameterized.class)
 public class TestDifferencasGrafia {
 
+    private final String nomeComDiferencaGrafica;
+    private final String nomeEsperado;
+    private DifferencasGrafia differencasGrafia;
+
+    public TestDifferencasGrafia(String nomeComDiferencaGrafica, String nomeEsperado) {
+        this.nomeComDiferencaGrafica = nomeComDiferencaGrafica;
+        this.nomeEsperado = nomeEsperado;
+    }
+
+    @Before
+    public void setup() {
+        this.differencasGrafia = new DifferencasGrafia();
+    }
+
+    @Parameters(name = "{index}: {0} -> {1}")
+    public static Collection<Object[]> dados() {
+        return Arrays.asList(new Object[][] {
+            { "Monica Hirata Sant`anna", "Mônica Hirata Sant'anna" },
+            { "Mônica Hirata Sant’anna", "Mônica Hirata Sant'anna" },
+            { "Sergio Henrique Guaraldi", "Sérgio Henrique Guaraldi" },
+            { "Sérgio Henrique Guaraldi", "Sérgio Henrique Guaraldi" }
+        });
+    }
+
+    @Test
+    public void deveUnificarDiferencasTipograficasParaGrafiaCorreta() {
+        String nomeCorrigido = differencasGrafia.corrigirGrafia(nomeComDiferencaGrafica);
+        assertEquals(nomeEsperado, nomeCorrigido);
+    }
 }
