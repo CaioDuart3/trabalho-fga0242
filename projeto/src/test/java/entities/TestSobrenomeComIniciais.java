a3d8582c9a40f07ee8e2e9a117f2507a7ce67d44
felixlaryssa <felixllaryssa@gmail.com>
2026-06-10 11:06:52 -0400

    test: adiciona testes parametrizados para a classe SobrenomeComIniciais

--- projeto/src/test/java/entities/TestSobrenomeComIniciais.java
+++ projeto/src/test/java/entities/TestSobrenomeComIniciais.java
@@ -1,8 +1,54 @@
 package entities;
 
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import tppe.entities.SobrenomeComIniciais;
+
 /**
  * Suite de testes para o Caso 2: Sobrenome + Iniciais dos nomes
  */
+@RunWith(Parameterized.class)
 public class TestSobrenomeComIniciais {
 
+    private final String nomeVariante;
+    private final String nomeEsperado;
+    private SobrenomeComIniciais sobrenomeComIniciais;
+
+    public TestSobrenomeComIniciais(String nomeVariante, String nomeEsperado) {
+        this.nomeVariante = nomeVariante;
+        this.nomeEsperado = nomeEsperado;
+    }
+
+    @Before
+    public void setup() {
+        this.sobrenomeComIniciais = new SobrenomeComIniciais();
+    }
+
+    @Parameters(name = "{index}: {0} -> {1}")
+    public static Collection<Object[]> dados() {
+        return Arrays.asList(new Object[][] {
+            { "Seabra A M", "Ana de Mattos Seabra" },
+            { "Seabra A. M.", "Ana de Mattos Seabra" },
+            { "Ana de Mattos Seabra", "Ana de Mattos Seabra" },
+            { "Souza C.", "Cassius de Souza" },
+            { "Souza C", "Cassius de Souza" }
+        });
+    }
+
+    @Test
+    public void deveUnificarSobrenomeComIniciaisParaFormaCompleta() {
+        String nomeUnificado = sobrenomeComIniciais.unificarNome(nomeVariante);
+        assertEquals(nomeEsperado, nomeUnificado);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deveLancarExcecaoQuandoNomeForNulo() {
+        sobrenomeComIniciais.unificarNome(null);
+    }
 }
